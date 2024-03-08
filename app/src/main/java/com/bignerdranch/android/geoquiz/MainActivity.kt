122d122347d78cdea74284fc91ed34f7da39d71d
jhernandez4 <jokhernandez120744@gmail.com>
2024-03-08 14:41:41 -0800

    Add click listener to question text view

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -46,6 +46,11 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        binding.questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
         updateQuestion()
     }
 
